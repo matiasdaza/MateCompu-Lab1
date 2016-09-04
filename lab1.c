6117fe23b0e970aca8811141bcb9b8bf4bd30a60
matiasdaza <m.daza6@gmail.com>
2016-09-04 19:10:21 -0300

    Primeras lineas de prueba del codigo en C

---
+++ lab1.c
@@ -0,0 +1,10 @@
+//Programa que resuelve factoriales
+#include <stdio.h>
+
+main(){
+	int a,b,c; 
+	a = 3;
+	printf("\n Valor de a: %i.\n",a); //Sintaxis para mostarar un valor
+	printf("\n Hola mundo, probando compilacion en Linux \n \n");
+
+}
