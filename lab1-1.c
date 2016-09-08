8b3cfc7dfcb5a66eaa67f7f62cf7581b51a351cb
matiasdaza <m.daza6@gmail.com>
2016-09-07 23:36:13 -0300

    Se declaran librerías time y gmp. Se agrega el mensaje del tiempo de duración de ejecución

--- lab1-1.c
+++ lab1-1.c
@@ -1,12 +1,16 @@
 //Programa que resuelve factoriales
 #include <stdio.h>
+#include <time.h> //Librería para medir el tiempo
+#include <gmp.h> //Librería para trabajo matemático
 
 int factorial(int n);
 
 
-main(){
+main()
+{
 	int n, k, facn,fack,dif,facdif, fac;
-
+	clock_t inicio, fin; // Se crea variable 'clock_t' para calcular el tiempo de ejecúción del programa.
+	inicio=clock();
 	printf("\nIntroduce un valor para n: ");
 	scanf("%i", &n); //%i el i es porque es un entero!!!
 	printf("\nIntroduce un valor para k: ");
@@ -16,23 +20,25 @@ main(){
 	printf("\nEl valor de la dif es: %i \n", dif);
 	facn=factorial(n);
 	fack=factorial(k);
-	facdif=factorial(dif);
+	facdif=factorial(dif); 	
 	printf("\nEl valor de la factorial de n es: %i \n", facn);
 	printf("\nEl valor de la factorial de k es: %i \n", fack);
 	printf("\nEl valor de la factorial de dif es: %i \n", facdif);
 
 	fac= facn / (fack*facdif); 
 	printf("\nEl valor del calculo de la combinatoria es: %i \n", fac);
-
+	fin=clock();
+	printf("El tiempo de ejecución fue de: %f \n",(fin-inicio)/(double)CLOCKS_PER_SEC); // %f es porque es un flotante!!!
 
 	return 0; 
 }
 
 int factorial(int n){
 	int fac=1;
-	while(n>1
-)	{
+	while(n>1)
+	{
 		fac=fac*n;
+		printf("%i\n", fac);
 		n=n-1;
 	}
 	return fac; 
