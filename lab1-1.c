8271bbf4eb3e32a06c4092dd8e8c72d2900003ac
matiasdaza <m.daza6@gmail.com>
2016-09-08 01:26:31 -0300

    Se cambia drasticamente el programa al agregar funciones de gmp

--- lab1-1.c
+++ lab1-1.c
@@ -3,43 +3,74 @@
 #include <time.h> //Librería para medir el tiempo
 #include <gmp.h> //Librería para trabajo matemático
 
-int factorial(int n);
+void combinatoria(int n, int k);
+
+//arcg: cantidad de parámetros contando el nombre del ejecutable que es el primer parámetro.
+//argv[]: Un arreglo que contiene todos los parámetros recibidos.
+//Lo hago para recibir los valores desde la consola!!!
 
 
-main()
+main(int argc, char *argv[])
 {
-	int n, k, facn,fack,dif,facdif, fac;
+	int n, k;
 	clock_t inicio, fin; // Se crea variable 'clock_t' para calcular el tiempo de ejecúción del programa.
 	inicio=clock();
-	printf("\nIntroduce un valor para n: ");
-	scanf("%i", &n); //%i el i es porque es un entero!!!
-	printf("\nIntroduce un valor para k: ");
-	scanf("%i", &k); //%i el i es porque es un entero!!!
+	n = atoi(argv[1]); // Se convierten los datos ingresados a variable tipo entero.
+	k = atoi(argv[2]);
 
-	dif=n-k;
-	printf("\nEl valor de la dif es: %i \n", dif);
-	facn=factorial(n);
-	fack=factorial(k);
-	facdif=factorial(dif); 	
-	printf("\nEl valor de la factorial de n es: %i \n", facn);
-	printf("\nEl valor de la factorial de k es: %i \n", fack);
-	printf("\nEl valor de la factorial de dif es: %i \n", facdif);
-
-	fac= facn / (fack*facdif); 
-	printf("\nEl valor del calculo de la combinatoria es: %i \n", fac);
+	if (k > n) // Como es un arreglo uno se guarda en la posición 1 y el otro en la 2.Si se ingresan mal los datos, el programa lo acusará y se cerrará.
+	{
+		printf("El valor de n tiene que ser mayor que el de k.\n"); // Si se ingresan mal los datos, el programa lo acusará y se cerrará.
+		return 0;
+	}
+	if (argc != 3) //COmo argc es la cantidad de parámetros, si lee más de 3 estará malo (porque solo necesitamos n y k)
+		printf("Para ejecutar use: ./Ejecutar [valor n] [valor k] \n"); // Si se ingresan más de dos argumentos en la terminal, el programa lo acusará y se cerrará.
+	else
+	{
+		
+		combinatoria(n,k); //Se manda los datos ingresados.
+	}
+	
 	fin=clock();
 	printf("El tiempo de ejecución fue de: %f \n",(fin-inicio)/(double)CLOCKS_PER_SEC); // %f es porque es un flotante!!!
 
 	return 0; 
 }
 
-int factorial(int n){
-	int fac=1;
-	while(n>1)
+void combinatoria(int n, int k)
+{
+	int i;
+	mpz_t facn, fack, facdif, denominador, combinatoriaFinal, dif; // mpz_t son variables del tipo entero
+	mpz_init (denominador); // mpz_init lo uso para inicializar la variable en 0.
+	mpz_init (combinatoriaFinal);
+	mpz_init_set_ui(facn,1); // mpz_init_set_ui lo uso para inicializar la variable en 1.
+	mpz_init_set_ui(fack,1);
+	i=1;
+	while(i<=n)
 	{
-		fac=fac*n;
-		printf("%i\n", fac);
-		n=n-1;
+		mpz_mul_ui(facn,facn,i); // Para sacar el factorial de n, facn=facn*i
+		i++;
 	}
-	return fac; 
+	i=1;
+	while(i<=k)
+	{
+		mpz_mul_ui(fack,fack,i);
+		i++;
+	}
+	gmp_printf("%i! = %Zd \n", n, facn); //Esto dirá: n! = facn pero con los valores de la varible.
+	gmp_printf("%i! = %Zd \n", k, fack);
+	mpz_init_set_ui(facdif,1);
+	i=1;
+	while(i<=(n-k))
+	{
+		mpz_mul_ui(facdif,facdif,i); // Para sacar el factorial de la diferencia, facdif=facdif*i
+		i++;
+	}
+	printf("salio\n");
+	gmp_printf("(n-k)! = %Zd \n", facdif);
+	mpz_mul(denominador,facdif,fack); // Multiplicación para el denominador (n-k)! * k! 
+	gmp_printf("k!(n-k)! = %Zd \n", denominador); // Se imprime la multiplicación de (n-k)! * k!
+	mpz_cdiv_q(combinatoriaFinal,facn,denominador);
+	gmp_printf("El resultado de la combinatoria es = %Zd \n", combinatoriaFinal); 
+
 }
